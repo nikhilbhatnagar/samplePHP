850d05bcbbe040a8e45b923ed5b1cfbb7ad4cfe0
Nikhil <nikhilbh@mail.com>
2015-11-13 11:27:38 +0800

    added ant script to copy files to server

--- web/index.php
+++ web/index.php
@@ -1,5 +1,14 @@
-<?php
-echo $_SERVER['HTTP_USER_AGENT'];
-echo "<br/> Hello World <br/> <br/>";
-phpinfo();
-?>
+<html>
+  <head>
+    <title>Welcome to firstphp.com!</title>
+  </head>
+  <body>
+    <h1>Success!  The firstphp.com virtual host is working!</h1>
+    <br/><br/>
+    
+    <?php
+		echo $_SERVER['HTTP_USER_AGENT'];
+		phpinfo();
+	?>
+  </body>
+</html>
